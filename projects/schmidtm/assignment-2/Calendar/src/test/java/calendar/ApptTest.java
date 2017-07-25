5564abbfefc7f0220c192d96d16e7d14955b74f8
Max Schmidt <cascadeth@gmail.com>
2017-07-24 19:45:59 -0700

    Completed assignment 2, not complete coverage

---
+++ projects/schmidtm/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -0,0 +1,120 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  Appt class.
+ */
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ApptTest {
+	/**
+    * Test that the gets methods work as expected.
+    */
+	@Test
+	public void test01()  throws Throwable  {
+		int startHour=13;
+		int startMinute=30;
+		int startDay=10;
+		int startMonth=9;
+		int startYear=2017;
+		String title="Work LAN";
+		String description="This is the workplace end of summer LAN party";
+
+		// Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+			startMinute ,
+		    startDay ,
+		    startMonth ,
+			startYear ,
+			title,
+			description);
+
+		// Assertions
+
+        // Check validity first
+		assertTrue(appt.getValid());
+
+		// Check set correctly in constructor with getters
+		assertEquals(13, appt.getStartHour());
+		assertEquals(30, appt.getStartMinute());
+		assertEquals(10, appt.getStartDay());
+		assertEquals(9, appt.getStartMonth());
+		assertEquals(2017, appt.getStartYear());
+		assertEquals("Work LAN", appt.getTitle());
+		assertEquals("This is the workplace end of summer LAN party", appt.getDescription());
+
+		// Make sure we can edit the appointment with setters, and check each one
+        appt.setStartHour(18);
+        assertEquals(18, appt.getStartHour());
+
+        appt.setStartMinute(31);
+        assertEquals(31, appt.getStartMinute());
+
+        appt.setStartDay(11);
+        assertEquals(11, appt.getStartDay());
+
+        appt.setStartMonth(10);
+        assertEquals(10, appt.getStartMonth());
+
+        appt.setStartYear(2020);
+        assertEquals(2020, appt.getStartYear());
+
+        appt.setTitle("Pushed back a ways...");
+        assertEquals("Pushed back a ways...", appt.getTitle());
+
+        appt.setDescription("Everyone went on strike. Now we can't LAN...");
+        assertEquals("Everyone went on strike. Now we can't LAN...", appt.getDescription());
+
+        // Default constructor should leave appt with no recurrence, check all getters
+        assertFalse(appt.isRecurring());
+        assertEquals(0, appt.getRecurDays().length);
+        assertEquals(0, appt.getRecurIncrement());
+        assertEquals(0, appt.getRecurNumber());
+	}
+
+
+	// Heavily test validity after trying to make appt invalid
+	@Test
+	public void testApptValidity()  throws Throwable  {
+
+	    Appt breakThisAppt = new Appt(1,2,3,4,5,"Breakable","Going to destroy this appointment... :D");
+
+	    // Break and then reset each
+
+        breakThisAppt.setStartHour(57);
+        assertFalse(breakThisAppt.getValid());
+
+        breakThisAppt.setStartHour(0); // Reset to valid
+        assertTrue(breakThisAppt.getValid());
+
+        breakThisAppt.setStartMinute(9999);
+        assertFalse(breakThisAppt.getValid());
+
+        breakThisAppt.setStartMinute(0); // Reset to valid
+        assertTrue(breakThisAppt.getValid());
+
+        breakThisAppt.setStartDay(-1);
+        assertFalse(breakThisAppt.getValid());
+
+        breakThisAppt.setStartDay(0); // Reset to valid
+        assertTrue(breakThisAppt.getValid());
+
+        breakThisAppt.setStartMonth(13);
+        assertFalse(breakThisAppt.getValid());
+
+        breakThisAppt.setStartMonth(0); // Reset to valid
+        assertTrue(breakThisAppt.getValid());
+
+        breakThisAppt.setStartYear(-1);
+        assertFalse(breakThisAppt.getValid());
+
+        breakThisAppt.setStartYear(2017); // Reset to valid
+        assertTrue(breakThisAppt.getValid());
+
+
+
+	}
+
+	//add more unit tests as you needed
+	
+}
---
+++ projects/schmidtm/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -0,0 +1,78 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  CalDay class.
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+import org.junit.Test;
+import sun.java2d.loops.GeneralRenderer;
+
+import static org.junit.Assert.*;
+
+public class CalDayTest {
+
+    // Try to add a bad appt
+	@Test
+	public void testCreatedInvalidAppt()  throws Throwable  {
+
+	    GregorianCalendar test = new GregorianCalendar();
+
+	    CalDay sooperDay = new CalDay(test);
+
+	    Appt awesomeAppt = new Appt(-57, 97, 57, -1, 0,"Lol this would never work...", "We really don' goof trying to add this... :D");
+
+	    sooperDay.addAppt(awesomeAppt);
+
+	    // Check if the list actually behaves and size is 0
+	    assertEquals(0, sooperDay.getSizeAppts());
+    }
+
+
+
+    // Test the iterator and toString methods on incomplete objects
+	@Test
+	public void testItrAndtoString()  throws Throwable {
+
+	    GregorianCalendar test = new GregorianCalendar();
+
+	    // Valid is false on default
+	    CalDay testDay = new CalDay();
+
+	    // CalDay constructed but no appointments, cannot return
+	    testDay.iterator();
+
+	    // Since there is nothing in the CalDay and it's false to begin with, this will not error but not be caught...
+        testDay.toString();
+    }
+
+    // Test adding appointment
+    @Test
+    public void testAddAppt() throws Throwable {
+
+        GregorianCalendar current = new GregorianCalendar(2017,7,22);
+        CalDay today = new CalDay(current);
+
+        Appt appt = new Appt(2,15,23, 7, 2017, "Out late getting food", "Getting sooper dank burritos with fam");
+
+        today.addAppt(appt);
+
+        // There should be one appointment now...
+        assertEquals(1, today.getSizeAppts());
+
+    }
+
+    @Test
+    public void testSettersWithGetters() throws Throwable {
+
+        GregorianCalendar current = new GregorianCalendar(2017,7,22);
+        CalDay today = new CalDay(current);
+
+        assertEquals(2017, today.getYear());
+        assertEquals(7, today.getMonth());
+        assertEquals(22, today.getDay());
+    }
+
+}
